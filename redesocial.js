7c4b82176dda99d30d2cc9086a552cedad66a919
Lucas Silveira <lucasxd010@hotmail.com>
2021-08-17 21:57:31 -0300

    17-08

---
+++ redesocial.js
@@ -0,0 +1,40 @@
+let gerenciaRedesSocial = () => {
+    let vetorRedesSociais = []
+    for(let i=0;i<5;i++){
+        let objeto = {
+            codigo: Number(prompt(`Informe o código da rede social`)),
+            nome: prompt(`Informe nome da rede social`),
+            url: prompt(`Informe a url da rede`)
+        }
+        // inserir objeto no vetor
+        vetorRedesSociais.push(objeto)
+    }
+}
+// cadastro dos usuarios 
+let vetorUsuarios = []
+for(let i=0;i<5;i++){
+    let objeto = {
+        login: prompt(`Informe o login do usuário`),
+        nome: prompt(`Informe o nome do usuário`),
+        codigoRedeSocial: Number(prompt(`Informe o código da rede social`)),
+        qtdePosts: Number(prompt(`Informe a qtde de Posts`))
+    }
+    // só faremos isso se a rede social existir
+    let achou = false // assumo que nao achei a rede social
+    let j = 0 // indice do vetor rede sociais
+    while(!achou && j<5){
+        if(vetorRedesSociais[j].codigo == objeto.codigoRedeSocial){
+            vetorUsuarios.push(objeto) // inserimos
+            achou = true
+            alert(`Usuário Inserido com sucesso`)
+        }
+        j++
+    }
+    if(!achou) {
+        alert(`Usuário não inserido pois a Rede social não foi encontrada`)
+    }
+}
+// Exe1) o usuário informa informa a rede social, e o programa retorna quantos posts foram feitos
+// Exe2) o programa retorna quantos posts foram feitos em todas as redes sociais
+// Exe3) o usuário informa informa o login do usuário, e o programa retorna quantos posts ele fez
+// Exe4) o programa retorna quantos posts foram feitos por cada usuário
