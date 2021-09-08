10a13cd31c8dc943636167f7cd7b73c2ccfdf38a
Lucas Silveira <lucasxd010@hotmail.com>
2021-09-08 19:54:35 -0300

    08/09

--- funcoes.js
+++ funcoes.js
@@ -1,8 +1,20 @@
 function principal(){
-
-    f1()
-    f2()
-    f3()
+    let opcao
+    do{
+        opcao = Number(prompt(`Digite 1. F1 2. F2 3. F3 4. Sair`))
+        switch(opcao){
+            case 1: f1()
+                    break
+            case 2: f2()
+                    break
+            case 3: f3()
+                    break
+            case 4: alert(`Programa será encerrado`)
+                    break
+            default: alert(`Opção Inválida`)
+        }
+    }
+    while(opcao !== 4)
 }
 
 function f1(){
