ffbddbfeb9afda9341106e2e113196bd955dc28c
Lucas Silveira <lucasxd010@hotmail.com>
2021-09-08 19:46:05 -0300

    08/09

---
+++ funcoes.js
@@ -0,0 +1,18 @@
+function principal(){
+
+    f1()
+    f2()
+    f3()
+}
+
+function f1(){
+    alert(`Chamada de f1`)
+}
+
+function f2() {
+    alert(`Chamada de f2`)
+}
+
+function f3() {
+    alert(`Chamada de f3`)
+}
